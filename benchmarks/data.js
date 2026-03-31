135370ff1be6feff64f38c0f949f504daef17e30
github-action-benchmark <github@users.noreply.github.com>
2026-03-31 12:52:01 +0000

    add n8n-mcp Benchmarks (customSmallerIsBetter) benchmark result for 8145669800aab8c6522e416182b8a5ebd6c26ef3

---
+++ benchmarks/data.js
@@ -0,0 +1,39 @@
+window.BENCHMARK_DATA = {
+  "lastUpdate": 1774961521134,
+  "repoUrl": "https://github.com/VortexZen-Security/n8n-mcp",
+  "entries": {
+    "n8n-mcp Benchmarks": [
+      {
+        "commit": {
+          "author": {
+            "email": "percy@railway.app",
+            "name": "Percy",
+            "username": "railway-bot"
+          },
+          "committer": {
+            "email": "percy@railway.app",
+            "name": "Percy",
+            "username": "railway-bot"
+          },
+          "distinct": true,
+          "id": "8145669800aab8c6522e416182b8a5ebd6c26ef3",
+          "message": "Initial commit",
+          "timestamp": "2026-03-31T12:49:28Z",
+          "tree_id": "880b61626575287f5483921366a8700eb8c747f5",
+          "url": "https://github.com/VortexZen-Security/n8n-mcp/commit/8145669800aab8c6522e416182b8a5ebd6c26ef3"
+        },
+        "date": 1774961515319,
+        "tool": "customSmallerIsBetter",
+        "benches": [
+          {
+            "name": "sample - array sorting - small",
+            "value": 0.0136,
+            "range": "0.3096",
+            "unit": "ms",
+            "extra": "73341 ops/sec"
+          }
+        ]
+      }
+    ]
+  }
+}
